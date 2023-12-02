8083b868b14897f9139e1e8a6bf03bdc1ec7c5b9
Karlthegodvinu <146846868+Karlthegodvinu@users.noreply.github.com>
2023-12-03 01:08:26 +0530

    Add files via upload

--- Calculator_3options.py
+++ Calculator_3options.py
@@ -1,79 +1,88 @@
-print("**********CALCULATOR**********")
 import math
 x=math.pi
+choice = True
+while choice:
+    print("**********CALCULATOR**********")
+    print("1. Basic Math \n2. Trigonometry \n3. Multiplication Table \n4. Factorial")
 
-print("1. Basic Math \n2. Trigonometry \n3. Multiplication Table \n4. Factorial")
+    n=int(input("Enter the value of n: "))
 
-n=int(input("Enter the value of n: "))
-
-if n==1:
-    print("1.Addition \n2.Subsraction \n3.Product \n4.Division")
-    a=int(input("Enter the value: "))
-
-    c=int(input("Value of the 1st number: "))
-    d=int(input("Value of the 2nd numnber: "))
-    if a==1:
-        x=c+d
-        print(f"Addition of {c} and {d} is {x}")
-    elif a==2:
-        x=c-d
-        print(f"Substraction of {c} and {d} is {x}")
-    elif a==3:
-        x=c*d
-        print(f"Product of {c} and {d} is {x}")
-    elif a==4:
-        x=c/d
-        print(f"Division of {c} and {d} is {x}")
-    else:
-        print("There is no option like that")
-
-elif n==2:
-    print("1. sin \n2. cos \n3. tan \n4. cosec \n5. sec \n6. cot")
-
-    c=int(input("Enter the value: "))
-
-
-    a=int(input("Enter the value of c is the numerator : "))
-    b=int(input("Enter the value of d is the denominator : "))
-    if c==1:
-        sin=math.sin(a*x/b)
-        print(format(sin,'.2f'))
-    elif c==2:
-        cos=math.cos(a*x/b)
-        print(format(cos,'.2f'))
-    elif c==3:
-        tan=math.tan(a*x/b)
-        print(format(tan,'.2f'))
-    elif c==4:
-        cosec=1/math.sin(a*x/b)
-        print(format(cosec,'.2f'))
-    elif c==5:
-        sec=1/math.cos(a*x/b)
-        print(format(sec,'.2f'))
-    elif c==6:
-        cot=1/math.tan(a*x/b)
-        print(format(cot,'.2f'))
-    else:
-        print("There is no option like that")
-
-elif n==3:
-    num=int(input("Enter the number whose table is to be given : "))
-    for i in range(1,11):
-        print(num,' x ',i,' = ',num*i)
-elif n==4:
-    a=int(input("Enter the value for calculating Facotrial: "))
-    def factorial(a):
-        if a==0:
-            return 1
+    if n==1:
+        print("1.Addition \n2.Subsraction \n3.Product \n4.Division")
+        a=int(input("Enter the value: "))
+        if 1<=a<=4:
+            c=int(input("Value of the 1st number: "))
+            d=int(input("Value of the 2nd numnber: "))
+            if a==1:
+                x=c+d
+                print(f"Addition of {c} and {d} is {x}")
+            elif a==2:
+                x=c-d
+                print(f"Substraction of {c} and {d} is {x}")
+            elif a==3:
+                x=c*d
+                print(f"Product of {c} and {d} is {x}")
+            elif a==4:
+                x=c/d
+                print(f"Division of {c} and {d} is {x}")
         else:
-            return a* factorial(a-1)
+            print("There is no option like that.")
 
-    result=factorial(a)
-    print(f"The factorial of {a} is {result}")
+    elif n==2:
+        print("1. sin \n2. cos \n3. tan \n4. cosec \n5. sec \n6. cot")
 
-else:
-    print("There is no option like that")
-    
-        
-        
-        
+        c=int(input("Enter the value: "))
+
+        if 1<=c<=6:
+            a=int(input("Enter the value of c is the numerator : "))
+            b=int(input("Enter the value of d is the denominator : "))
+            if c==1:
+                sin=math.sin(a*x/b)
+                print(format(sin,'.2f'))
+            elif c==2:
+                cos=math.cos(a*x/b)
+                print(format(cos,'.2f'))
+            elif c==3:
+                tan=math.tan(a*x/b)
+                print(format(tan,'.2f'))
+            elif c==4:
+                cosec=1/math.sin(a*x/b)
+                print(format(cosec,'.2f'))
+            elif c==5:
+                sec=1/math.cos(a*x/b)
+                print(format(sec,'.2f'))
+            elif c==6:
+                cot=1/math.tan(a*x/b)
+                print(format(cot,'.2f'))
+            else:
+                print("There is no option like that")
+        else:
+            print("There is no option like that")
+
+    elif n==3:
+        num=int(input("Enter the number whose table is to be given : "))
+        for i in range(1,11):
+            print(num,' x ',i,' = ',num*i)
+    elif n==4:
+        a=int(input("Enter the value for calculating Facotrial: "))
+        def factorial(a):
+            if a==0:
+                return 1
+            else:
+                return a* factorial(a-1)
+
+        result=factorial(a)
+        print(f"The factorial of {a} is {result}")
+
+    else:
+        print("There is no option like that")
+
+    print('Do you wish to continue?(y/n)')
+    cont= input()
+    if cont == 'y':
+        choice = True
+    elif cont == 'n':
+        choice = False
+    else:
+        print('Invalid Command, please recheck the spelling.')
+        cont='n'
